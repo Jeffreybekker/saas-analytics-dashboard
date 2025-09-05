546aec8ae30a3b3f385f866fb036f3c08147925d
Jeffrey Bekker <120750708+Jeffreybekker@users.noreply.github.com>
2025-09-05 04:24:42 +0200

    Created the events model and basic admin.py, indexes come later!

--- events/admin.py
+++ events/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("event_type", "user", "timestamp")
+    list_filter = ("event_type", "timestamp")
+    search_fields = ("user__email", "metadata")
